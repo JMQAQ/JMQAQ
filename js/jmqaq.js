7d1588a013c6c44897767943d03aceea49172160
JMQAQ <1149592618@qq.com>
2024-02-29 02:22:02 +0000

    Site updated: 2024-02-29 02:22:02

---
+++ js/jmqaq.js
@@ -0,0 +1,20 @@
+// var now = new Date();
+// function createtime(){
+//     var grt = new Date(xb.ctime);
+//     now.setTime(now.getTime()+250);
+//     days = (now-grt)/1000/60/60/24;dnum = Math.floor(days);
+//     hours = (now-grt)/1000/60/60-(24*dnum);hnum = Math.floor(hours);
+//     if(String(hnum).length==1){hnum = '0'+hnum;}
+//     minutes = (now-grt)/1000/60-(24*60*dnum)-(60*hnum);mnum = Math.floor(minutes);
+//     if(String(mnum).length==1){mnum = '0'+mnum;}
+//     seconds = (now-grt)/1000-(24*60*60*dnum)-(60*60*hnum)-(60*mnum);snum = Math.round(seconds);
+//     if(String(snum).length==1){snum = '0'+snum;}
+//     document.getElementById('span_dt_dt').innerHTML = dnum+'天'+hnum+'小时'+mnum+'分'+snum+'秒';
+// }
+// setInterval('createtime()',250);
+// //console
+
+console.clear();
+console.log('项目托管:https://github.com/');
+console.log('%Hallo World!','font-size:2em');
+console.log('%c页面加载完毕消耗了'+Math.round(performance.now()*100)/100+'ms','background:#fff;color:#333;text-shadow:0 0 2px #eee,0 0 3px #eee,0 0 3px #eee,0 0 2px #eee,0 0 3px #eee;');
